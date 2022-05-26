14c1569d3912bc9f82c2f0e625fda178bd46706f
shwld <shu.account@outlook.com>
2022-05-26 16:12:25 +0900

    comment取得

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,42 @@
 import * as core from '@actions/core'
 // import {Octokit} from '@octokit/rest'
-import {readFileSync} from 'node:fs'
+// import {WebClient} from '@slack/web-api'
+import {readFileSync} from 'fs'
 
-type GitHubEventMessage = {
-  body: string
-  id: string
-  node_id: string
-  original_commit_id: string
-  original_line: number
-  original_position: number
-  original_start_line: number
-  path: string
-  position: number
-  pull_request_review_id: string
-  diff_hunk: string
+type GitHubEvent = {
+  comment: {
+    body: string
+    id: string
+    node_id: string
+    original_commit_id: string
+    original_line: number
+    original_position: number
+    original_start_line: number
+    path: string
+    position: number
+    pull_request_review_id: string
+    diff_hunk: string
+  }
 }
 
 async function run(): Promise<void> {
   try {
     const gitHubToken: string = core.getInput('github_token')
     const gitHubEventPath: string = core.getInput('github_event_path')
+    // const slackToken: string = core.getInput('slack_token')
     core.debug(`Token is ${gitHubToken} ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    const gitHubEvent: GitHubEventMessage = JSON.parse(
-      readFileSync(gitHubEventPath, 'utf8')
-    )
+    const gitHubEventText = readFileSync(gitHubEventPath, {
+      encoding: 'utf-8'
+    }).toString()
+    const gitHubEvent: GitHubEvent = JSON.parse(gitHubEventText)
+    // core.debug(JSON.stringify(gitHubEvent.comment))
 
-    if (!gitHubEvent.body.includes('[mofmof]')) {
+    if (!gitHubEvent.comment.body.includes('[mofmof]')) {
       core.debug('No [mofmof] found in body')
-      return
+      return core.setOutput('time', new Date().toTimeString())
     }
 
-    // gitHubEvent.comment.id
-    core.debug(gitHubEvent.toString())
-    // core.debug(new Date().toTimeString())
     core.debug(new Date().toTimeString())
 
     // const octokit = new Octokit({
@@ -46,6 +49,13 @@ async function run(): Promise<void> {
     // })
 
     // core.debug(JSON.stringify(res))
+
+    // const web = new WebClient(slackToken)
+
+    // web.chat.postMessage({
+    //   text: 'Hello world!',
+    //   channel: '#work'
+    // })
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
