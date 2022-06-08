cd3f29ecc48bc2318ced093b7ea016a932554ba9
shwld <shu.account@outlook.com>
2022-06-08 15:45:34 +0900

    try

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -22,7 +22,7 @@ import {expect, test} from '@jest/globals'
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const ip = path.join(__dirname, '..', '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
     env: process.env
   }
