fafbf588c3f7e3a3b9de79045f1d1a6736f0fc43
shwld <shu.account@outlook.com>
2022-06-24 12:51:21 +0900

    ✨feat: awesome code to slack action

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,29 +1,36 @@
-import {wait} from '../src/wait'
+import {config} from 'dotenv'
+config()
+
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 import {expect, test} from '@jest/globals'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
+// test('throws invalid number', async () => {
+//   const input = parseInt('foo', 10)
+//   await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+// })
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
+// test('wait 500 ms', async () => {
+//   const start = new Date()
+//   await wait(500)
+//   const end = new Date()
+//   var delta = Math.abs(end.getTime() - start.getTime())
+//   expect(delta).toBeGreaterThan(450)
+// })
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
+  const env = {
+    INPUT_TRIGGER_KEYWORD: '[mofmof]',
+    INPUT_GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+    INPUT_GITHUB_EVENT_PATH: process.env.GITHUB_EVENT_PATH,
+    INPUT_SLACK_TOKEN: process.env.SLACK_TOKEN,
+    INPUT_SLACK_CHANNEL: process.env.SLACK_CHANNEL
   }
+  const options: cp.ExecFileSyncOptions = {env}
+  console.log({ip, np, __dirname, options: Object.keys(process.env)})
   console.log(cp.execFileSync(np, [ip], options).toString())
 })
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,166 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {Buffer} from 'buffer'
+import {Octokit} from '@octokit/rest'
+import {WebClient} from '@slack/web-api'
+import {readFileSync} from 'fs'
+
+type GitHubEvent = {
+  comment: {
+    _links: {
+      html: {
+        href: string
+      }
+      pull_request: {
+        href: string
+      }
+      self: {
+        href: string
+      }
+    }
+    body: string
+    id: string
+    node_id: string
+    original_commit_id: string
+    original_line: number
+    original_position: number
+    original_start_line: number
+    path: string
+    position: number
+    pull_request_review_id: string
+    diff_hunk: string
+    name: string
+    html_url: string
+    user: {
+      avatar_url: string
+      login: string
+    }
+    commit_id: string
+  }
+  sender: {
+    avatar_url: string
+    html_url: string
+    login: string
+  }
+  repository: {
+    name: string
+    owner: {
+      login: string
+    }
+  }
+  pull_request?: {
+    head: {
+      ref: string
+    }
+  }
+}
 
 async function run(): Promise<void> {
+  core.debug(`start with ${JSON.stringify(Object.keys(process.env))}`)
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const triggerKeyword: string = core.getInput('trigger_keyword')
+    const githubToken: string = core.getInput('github_token')
+    const githubEventPath: string = core.getInput('github_event_path')
+    const slackToken: string = core.getInput('slack_token')
+    const slackChannel: string = core.getInput('slack_channel')
+    core.debug(`getInput end, token is ${githubToken ? 'present' : 'null'}`)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const githubEventText = readFileSync(githubEventPath, {
+      encoding: 'utf-8'
+    }).toString()
+    const githubEvent: GitHubEvent = JSON.parse(githubEventText)
 
+    core.debug(JSON.stringify(githubEvent))
+
+    if (!githubEvent.comment.body.includes(triggerKeyword)) {
+      core.info(`No ${triggerKeyword} found in body`)
+      return core.setOutput('time', new Date().toTimeString())
+    }
+
+    core.debug(`${triggerKeyword} found`)
+
+    const octokit = new Octokit({
+      auth: githubToken
+    })
+
+    core.debug(`octokit initialized`)
+
+    const res = await octokit.rest.repos.getContent({
+      owner: githubEvent.repository.owner.login,
+      repo: githubEvent.repository.name,
+      path: githubEvent.comment.path,
+      ref: githubEvent.pull_request?.head.ref
+    })
+
+    core.debug(`octokit response is ${JSON.stringify(res)}`)
+    // core.debug(JSON.stringify(res.data))
+
+    // @ts-ignore
+    core.debug(`octokit res data content is ${res.data.content}`)
+
+    // @ts-ignore
+    const content = Buffer.from(res.data.content ?? '', 'base64').toString()
+    const lines = content
+      .split('\n')
+      .filter(
+        (_line, index) =>
+          index >= (githubEvent.comment.original_start_line ?? 0) &&
+          index <= githubEvent.comment.original_line
+      )
+
+    core.debug(lines.join('\n'))
+
+    const web = new WebClient(slackToken)
+    core.debug(`web client initialized`)
+
+    const slackResponse = await web.chat.postMessage({
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `*<${githubEvent.sender.html_url}|${
+              githubEvent.sender.login
+            }> found a recommended code!*\n ${githubEvent.comment.body.replace(
+              triggerKeyword,
+              ''
+            )}`
+          },
+          accessory: {
+            type: 'image',
+            image_url: githubEvent.sender.avatar_url,
+            alt_text: githubEvent.sender.login
+          }
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `
+    \`\`\`
+    ${lines.join('\n')}
+    \`\`\`
+                    `
+          }
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `<${githubEvent.comment.html_url}|${githubEvent.comment.commit_id}>`
+          }
+        }
+      ],
+      channel: `#${slackChannel}`
+    })
+
+    core.debug(JSON.stringify(slackResponse))
+    core.debug(`slack response is ${JSON.stringify(slackResponse)}`)
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.debug(JSON.stringify(error.stack))
+      core.setFailed(error.message)
+    }
   }
 }
 
